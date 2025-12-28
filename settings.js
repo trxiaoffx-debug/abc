1fd6d7ef0872e73a2ea2dc0994e10bdc020d16aa
trxiaoffx-debug <trxiaoffx@gmail.com>
2025-12-28 13:22:35 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 
 SESSION_ID: process.env.SESSION_ID === undefined ? 'dd5add0097c84d9a' : process.env.SESSION_ID,
-PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
+PORT: process.env.PORT === undefined ? "10000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "Rakitha" : process.env.SESSION_NAME,
 };
